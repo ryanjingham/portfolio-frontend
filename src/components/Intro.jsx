67b098fa3beb515737d704c04e1d04707d545b7e
Ryan Ingham <ryanjingham@gmail.com>
2024-10-31 20:05:16 +1100

    new headline

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,7 +6,7 @@ function Intro() {
         justify-center flex-col text-center pt-20 pb-6">
             <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">R. J.   Ingham</h1>
             <p className="text-base md:text-xl mb-3 font-medium">Software Engineer</p>
-            <p className="text-sm max-w-xl mb-6 font-bold">Australian-born and living in the UK, I run a contracting firm with projects spanning finance, eco-friendly waste management, and healthcare. My primary skillsets of Machine Learning and web app development allow me to work on a broad range of projects. I adapt quickly, ensuring seamless integration into any team or project with a practical focus on solving problems.</p>
+            <p className="text-sm max-w-xl mb-6 font-bold">Adaptable Software Engineer, Web Deveoper and ML Specialist, delivering innovative solutions across finance, healthcare, and sustainability. Based in Newcastle, NSW, I lead projects with a practical focus on seamless integration and impactful problem-solving.</p>
         </div>
     )
 }
